b21d0bf650042edc27874b78b7632de5b485c3f7
selvagsz <selvaganeshbsg@gmail.com>
2016-09-02 01:20:25 +0530

    Mobile optimised

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -3,13 +3,12 @@ import BarChart from 'github-search/viz/bar-chart';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    debugger;
     this._super(...arguments);
     d3.select(`#${this.elementId}`)
       .datum(this.get('data'))
       .call(
         BarChart()
-          .width(300)
+          .width(this.$().parent().width())
           .height(105)
           .margin(0)
           .color('#f6f6f6')
